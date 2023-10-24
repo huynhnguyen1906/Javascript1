9c9d93488ecf450daa71ae39a3bf595c0ecd8835
グエン　ドゥック　フィン <duchuynhnguyen1906@gmail.com>
2023-10-24 14:44:05 +0900

    kadai05 fix

--- js/kadai05.js
+++ js/kadai05.js
@@ -1,13 +1,10 @@
-for (i=10; i>0; i--){
-    console.log(i)
-}
 
 const blankLink = document.querySelectorAll("a");
 
 for (let i = 0; i < blankLink.length; i++) {
     blankLink[i].addEventListener("click", function(event) {
         event.preventDefault();
-        const href = blankLink[i].href;
-        window.open(href, "kadai05");
+        const link = blankLink[i].href;
+        window.open(link, "kadai05");
     }) 
 }
