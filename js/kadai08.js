d95c67eaf9e1ca246c52646f6c4842668fc5ee45
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2023-11-12 13:40:32 +0900

    add kadai 08

---
+++ js/kadai08.js
@@ -0,0 +1,25 @@
+const slider = document.querySelectorAll("#slider li");
+const nextBtn = document.getElementById("next");
+const prevBtn = document.getElementById("prev");
+let currentIndex = 0
+
+function showSlider(index) {
+    slider.forEach((slide, i) => {
+        slide.style.display = i === index ? "block" : "none";
+    });
+}
+
+setInterval(() => {
+    currentIndex = (currentIndex + 1 ) % slider.length; 
+    showSlider(currentIndex);
+}, 3000)
+
+nextBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex + 1 ) % slider.length; 
+    showSlider(currentIndex);
+})
+
+prevBtn.addEventListener("click", () => {
+    currentIndex = (currentIndex - 1 + slider.length) % slider.length;
+    showSlider(currentIndex);
+})
