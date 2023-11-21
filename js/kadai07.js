47c4adfc921442a6dd317d97b9e065af0bb02501
グエン　ドゥック　フィン <duchuynhnguyen1906@gmail.com>
2023-11-21 13:34:53 +0900

    Add animation when switch slide When slide transition, opacity switch between 0 and 1

--- js/kadai07.js
+++ js/kadai07.js
@@ -3,11 +3,30 @@ const nextBtn = document.getElementById("next");
 const prevBtn = document.getElementById("prev");
 let currentIndex = 0
 
+
 function showSlider(index) {
-    slider.forEach((slide, i) => {
-        slide.style.display = i === index ? "block" : "none";
-    });
+    for( i = 0; i < slider.length; i++ ) {
+        if (currentIndex === i) {
+            slider[i].style.display = 'block'
+            setTimeout(() => {
+                for( i = 0; i < slider.length; i++ ) {
+                    if (currentIndex === i) {
+                    slider[i].style.opacity = '1'
+                    }
+                }
+            })
+        }else {
+            slider[i].style.display = 'none'
+            slider[i].style.opacity = '0'
+        }
+    }
 }
+
+// function showSlider(index) {
+//     slider.forEach((slide, i) => {
+//         slide.style.display = i === index ? "block" : "none";
+//     });
+// }
 //JavaScriptでの三項演算子（ternary operator）は、条件をチェックし、その条件に基づいて値を返すことを可能に します。三項演算子の書き方のルールは以下の通りです：
 
 // 例えば
