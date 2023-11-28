674fe83b2172573bcc292f94fcddf9f754ceb147
グエン　ドゥック　フィン <duchuynhnguyen1906@gmail.com>
2023-11-28 14:28:46 +0900

    Fix kadai08... ..When display change block-none opacity change 1-0 When click prev, next button clear iterval

--- js/kadai08.js
+++ js/kadai08.js
@@ -3,23 +3,38 @@ const nextBtn = document.getElementById("next");
 const prevBtn = document.getElementById("prev");
 let currentIndex = 0
 
-function showSlider(index) {
-    slider.forEach((slide, i) => {
-        slide.style.display = i === index ? "block" : "none";
-    });
+function showSlider() {
+    for( i = 0; i < slider.length; i++ ) {
+        if (currentIndex === i) {
+            slider[i].style.display = 'block'
+            setTimeout(() => {
+                for( i = 0; i < slider.length; i++ ) {
+                    if (currentIndex === i) {
+                    slider[i].style.opacity = '1'
+                    }
+                }
+            }, 5)
+        }else {
+            slider[i].style.display = 'none'
+            slider[i].style.opacity = '0'
+        }
+    }
 }
 
-setInterval(() => {
-    currentIndex = (currentIndex + 1 ) % slider.length; 
-    showSlider(currentIndex);
-}, 3000)
 
 nextBtn.addEventListener("click", () => {
     currentIndex = (currentIndex + 1 ) % slider.length; 
-    showSlider(currentIndex);
+    showSlider();
+    clearInterval(autoSlider)
 })
 
 prevBtn.addEventListener("click", () => {
     currentIndex = (currentIndex - 1 + slider.length) % slider.length;
     showSlider(currentIndex);
+    clearInterval(autoSlider)
 })
+
+const autoSlider = setInterval(() => {
+    currentIndex = (currentIndex + 1 ) % slider.length; 
+    showSlider();
+}, 1500)
