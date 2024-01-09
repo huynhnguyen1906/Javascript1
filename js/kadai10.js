0e4c70e9cd9912677955705191bb0be37ad9874f
グエン　ドゥック　フィン <duchuynhnguyen1906@gmail.com>
2024-01-09 16:29:58 +0900

    Fix scroll behavior in naviBar

--- js/kadai10.js
+++ js/kadai10.js
@@ -1,16 +1,18 @@
 const naviBar = document.getElementById("category-navi")
-
+const naviBarTop = naviBar.getBoundingClientRect().top
 // scroll イベントのリスナーを追加
 window.addEventListener("scroll", () => {
 	// スクロールされた垂直位置を取得
 	const scrollY = window.scrollY
 
 	// もしスクロールされた位置がナビバーの上端よりも下ならば
-	if (scrollY > naviBar.getBoundingClientRect().top) {
+	if (scrollY > naviBarTop) {
 		// ナビバーの top プロパティをスクロール位置に合わせて更新
 		naviBar.style.top = scrollY + "px"
+		console.log("avs")
 	} else {
 		// スクロール位置がナビバーの上端よりも上ならば、ナビバーを画面上端に固定
 		naviBar.style.top = "0px"
 	}
+	console.log(scrollY)
 })
