4418debca55af52a8d72fc50f5314e800ea5e336
huynhnguyen1906 <duchuynhnguyen1906@gmail.com>
2024-08-07 13:57:51 +0900

    Refactor code to improve performance and readability

--- js/kadai13.js
+++ js/kadai13.js
@@ -1,34 +1,32 @@
-const slides = document.querySelectorAll('#slider li');
+const slides = document.querySelectorAll("#slider li");
+let currentIndex = -1;
 
-let currentIndex = -1
-
-for( i = 0; i < slides.length; i++) {
-    let zIndex = slides.length - i;
-    slides[i].style.zIndex = zIndex;
-    i = i++;
+for (let i = 0; i < slides.length; i++) {
+	let zIndex = slides.length - i;
+	slides[i].style.zIndex = zIndex;
 }
 
 function swipeSlider() {
-    for( i = 0; i < slides.length; i++ ) {
-        if (currentIndex === i) {
-            slides[i].classList.add('switch')
-        }else {
-            slides[i].classList.remove('switch')
-        }
-    }   
+	for (let i = 0; i < slides.length; i++) {
+		if (currentIndex === i) {
+			slides[i].classList.add("switch");
+		} else {
+			slides[i].classList.remove("switch");
+		}
+	}
 }
 
+function updateZIndex() {
+	const firstZIndex = parseInt(slides[slides.length - 1].style.zIndex);
+	for (let i = slides.length - 1; i > 0; i--) {
+		const prevZIndex = parseInt(slides[i - 1].style.zIndex);
+		slides[i].style.zIndex = prevZIndex;
+	}
+	slides[0].style.zIndex = firstZIndex;
+}
 
-
-setInterval( ()=> {
-    currentIndex = (currentIndex + 1) % slides.length;
-    swipeSlider();
-    setTimeout(()=> {
-        const firstZIndex = parseInt(slides[slides.length-1].style.zIndex)
-    for( i = slides.length - 1 ; i > 0 ; i-- ) {
-        const prevZIndex = parseInt(slides[i - 1].style.zIndex);
-        slides[i].style.zIndex = prevZIndex
-    }
-    slides[0].style.zIndex = firstZIndex
-    }, 1000)
-}, 1000)
+setInterval(() => {
+	currentIndex = (currentIndex + 1) % slides.length;
+	swipeSlider();
+	setTimeout(updateZIndex, 1000);
+}, 1000);
